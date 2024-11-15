6d13dce49e61cd8cd998adb9225bc3b81a0c1320
jimao <shihao.gu@perfma.com>
2024-11-15 16:23:20 +0800

    提交

--- src/app/magic/hello/route.ts
+++ src/app/magic/hello/route.ts
@@ -32,6 +32,11 @@ function screenDrawing(ctx: SKRSContext2D) {
   ctx.textAlign = 'right';
   ctx.fillText(`visits: ${3224334}`, 197, 2);
 
+  // 在屏幕中心添加 "Me"
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillText('Me', 100, 60);
+
   ctx.textAlign = 'left';
   ctx.textBaseline = 'bottom';
   ctx.fillText('>', 3, 120);
