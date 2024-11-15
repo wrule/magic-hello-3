81de26978ded09d394300784e7de01cb51ea90b0
jimao <shihao.gu@perfma.com>
2024-11-15 15:11:22 +0800

    提交

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push(...[
+      '@napi-rs/canvas',
+      '@napi-rs/canvas-android-arm64',
+      '@napi-rs/canvas-darwin-arm64',
+      '@napi-rs/canvas-darwin-x64',
+      '@napi-rs/canvas-linux-arm-gnueabihf',
+      '@napi-rs/canvas-linux-arm64-gnu',
+      '@napi-rs/canvas-linux-arm64-musl',
+      '@napi-rs/canvas-linux-x64-gnu',
+      '@napi-rs/canvas-linux-x64-musl',
+      '@napi-rs/canvas-win32-x64-msvc',
+    ]);
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/magic/hello/route.ts
+++ src/app/magic/hello/route.ts
@@ -1,8 +1,17 @@
 import { NextResponse } from 'next/server';
+import { createCanvas } from '@napi-rs/canvas';
 
 export
-const GET = (request: Request) => {
-  return NextResponse.json({
-    message: '你好，世界',
+const GET = async (request: Request) => {
+  const canvas = createCanvas(120, 40);
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#00ffff';
+  ctx.fillRect(0, 0, 20, 20);
+  const buffer = await canvas.encode('png');
+  return new Response(buffer, {
+    headers: {
+      'Content-Type': 'image/png',
+      'Content-Length': buffer.length.toString()
+    },
   });
 };
